a3a87b25df4327d15e772efce2f14f9eac72e91f
shankhara <boulenge.charles28@gmail.com>
2020-03-30 16:38:50 +0200

    new bs clearing

--- cbouleng/clear_backslash.c
+++ cbouleng/clear_backslash.c
@@ -1,5 +1,7 @@
 #include "minishell.h"
 
+char* map;
+
 int		bs_in_row(char* str, int* p_i)
 {
 	int	nb;
@@ -10,7 +12,7 @@ int		bs_in_row(char* str, int* p_i)
 		nb++;
 		(*p_i)++;
 	}
-	if (!(nb % 2)
+	if (!(nb % 2))
 		return (nb / 2);
 	return (nb / 2 + 1);
 }
@@ -28,6 +30,7 @@ int		nb_to_del(char* str)
 {
 	int	i;
 	int	sum;
+	int	nb;
 
 	i = 0;
 	sum = 0;
@@ -47,28 +50,40 @@ int		print_case(char* str, int* p_i)
 {
 	if (str[*p_i] == '\\')
 	{
-		if (str[*(p_i + 1)] == '|')
+		if (str[(*p_i + 1)] == '\\')
+		{
+			(*p_i)++;
 			return (1);
-		if (str[*(p_i + 1)] == '\\')
-			
+		}
+		if (map[*p_i] != '0')
+			return (1);
+		return (0);
 	}
 	return (1);
 }
 
-void	clear_it(char* str)
+char*	clear_it(char* str)
 {
 	int		nb;
 	char*	res;
 	int		i;
 
 	i = 0;
+	map = map_quote(str);
 	nb = nb_to_del(str);
 	if (!(res = malloc(ft_strlen(str) - nb + 1)))
 		ft_exit(1);
+	nb = 0;
 	while (str[i])
 	{
-			if (print_case(str, &i))
-
+		if (print_case(str, &i))
+			res[nb++] = str[i];
+		i++;
+	}
+	res[nb] = '\0';
+	free(map);
+	printf("[%s]\n", res);
+	return (res);
 }
 
 void	clear_backslash(void)
@@ -76,7 +91,10 @@ void	clear_backslash(void)
 	int	i;
 
 	i = 0;
-	clear_it(lst->cmd);
+	lst->cmd = clear_it(lst->cmd);
 	while (lst->arg[i])
-		clear_it(lst->arg[i++]);
+	{
+		lst->arg[i] = clear_it(lst->arg[i]);
+		i++;
+	}
 }
