19cd233fa331005b0837ff016954595fe908d93e
shankhara <boulenge.charles28@gmail.com>
2020-06-10 15:46:59 +0200

    tests2

--- wedding/parsing/main.c
+++ wedding/parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: cbouleng <cbouleng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/25 17:20:32 by cbouleng          #+#    #+#             */
-/*   Updated: 2020/05/25 17:20:34 by cbouleng         ###   ########.fr       */
+/*   Updated: 2020/06/10 12:17:25 by cbouleng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	parsing(char *line)
 {
 	char **stock;
 
+	printf("[%s]\n", line);
 	check(line);
 	stock = split_plus(line, ';');
 	list_it(stock);
